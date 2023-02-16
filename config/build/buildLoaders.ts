acf66d99c82449e43bd9d1d9b93db12430288cf1
daniil malina <skittymd@gmail.com>
2023-02-16 18:07:47 +0300

    подключил и настроил webpack-dev-server

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,13 +1,11 @@
 import { RuleSetRule } from 'webpack';
 
 export function buildLoaders(): RuleSetRule[] {
-
     const typesrciptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
     }
-
     return [
         typesrciptLoader
     ]
--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,11 +1,12 @@
 import webpack from 'webpack';
 import { BuildOptions } from "./types/config";
-import { buildLoaders } from './buildLoaders'; 
-import { buildPlugins } from './buildPlugins'; 
+import { buildLoaders } from './buildLoaders';
+import { buildPlugins } from './buildPlugins';
 import { buildResolvers } from './buildResolvers';
+import { buildDevServer } from './buildDevServer';
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-    const {paths, mode} = options;
+    const { paths, mode, isDev } = options;
     return {
         mode,
         entry: paths.entry,
@@ -18,6 +19,8 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
         module: {
             rules: buildLoaders()
         },
-        resolve: buildResolvers()
+        resolve: buildResolvers(),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined
     }
 }

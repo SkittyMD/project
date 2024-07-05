cdb7af3394a37b3e21bf5ff8e1de6cd37c6e5c05
skitty <skittymd@gmail.com>
2024-07-05 16:13:49 +0300

    добавлены темы стилей

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import { Suspense } from "react";
 import { Route, Routes, Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import './index.scss'
+import './styles/index.scss'
+import { useTheme } from "./theme/UseTheme";
+
 
 const App = () => {
+    const { theme, toggleTheme } = useTheme()
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
